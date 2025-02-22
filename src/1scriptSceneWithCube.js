223f748bc9aaa728bb719f6a56e61715ede333e3
anilkeshwani <anilkeshwani@hotmail.com>
2025-02-22 15:14:03 +0000

    Modifications: Spotlight, 2nd cube, 2nd material, remove plane, shift perspective (camera)

--- src/1scriptSceneWithCube.js
+++ src/1scriptSceneWithCube.js
@@ -18,8 +18,8 @@ const scene = new THREE.Scene();
 
 // Sizes
 const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
+    width: window.innerWidth,
+    height: window.innerHeight,
 };
 
 //////////////////////////////////////////
@@ -30,17 +30,32 @@ const sizes = {
 // Ambient light
 const ambientLight = new THREE.AmbientLight();
 ambientLight.color = new THREE.Color(0xffffff);
-ambientLight.intensity = 1;
+ambientLight.intensity = 0.25;
 scene.add(ambientLight);
 gui.add(ambientLight, "intensity").min(0).max(3).step(0.001);
 
 // Directional light
-const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.9);
-directionalLight.position.set(1, 0.25, 0);
-scene.add(directionalLight);
+// const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.9);
+// directionalLight.position.set(1, 0.25, 0);
+// scene.add(directionalLight);
 
-// Material -- shared between all geometires below
-const material = new THREE.MeshStandardMaterial();
+// Spotlight
+const light = new THREE.SpotLight(0xffffff);
+light.castShadow = true; // default false
+scene.add(light);
+
+// Materials -- shared between all geometires below
+const material = new THREE.MeshStandardMaterial({
+    color: 0x87CEEB, // light blue color
+    metalness: 0.2,
+    roughness: 0.1,
+});
+
+const material2 = new THREE.MeshBasicMaterial({
+    color: 0xff69b4, // pink color
+    metalness: 0.0,
+    roughness: 0.5,
+});
 
 // Geometries
 const planeGeometry = new THREE.PlaneGeometry(5, 5);
@@ -49,25 +64,29 @@ const geometrySphere = new THREE.SphereGeometry(0.5, 32, 32);
 
 // Meshes
 const cube = new THREE.Mesh(geometryBox, material);
+const cube2 = new THREE.Mesh(geometryBox, material2);
 
 const sphere = new THREE.Mesh(geometrySphere, material);
-sphere.position.x = -1.5;
+sphere.position.x = -1.0;
 
 const plane = new THREE.Mesh(planeGeometry, material);
 plane.rotation.x = -Math.PI * 0.5;
 plane.position.y = -0.65;
 
-scene.add(sphere, cube, plane);
+// scene.add(sphere, cube, plane);
+// scene.add(sphere, cube, cube2);
+scene.add(sphere, cube);
 
 // Camera
 const camera = new THREE.PerspectiveCamera(
-  75, // POV
-  sizes.width / sizes.height, // aspect ratio
-  0.1, // near
-  100 // far
+    75, // POV
+    sizes.width / sizes.height, // aspect ratio
+    0.01, // near
+    100 // far
 );
-camera.position.x = 1;
-camera.position.y = 1;
+
+camera.position.x = 5;
+camera.position.y = 4;
 camera.position.z = 2;
 scene.add(camera);
 
@@ -79,7 +98,7 @@ const controls = new OrbitControls(camera, canvas);
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
+    canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
@@ -90,23 +109,23 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 const clock = new THREE.Clock();
 
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
+    const elapsedTime = clock.getElapsedTime();
 
-  // Update objects
-  cube.rotation.y = 0.1 * elapsedTime;
-  cube.rotation.x = 0.15 * elapsedTime;
+    // Update objects
+    cube.rotation.y = 0.1 * elapsedTime;
+    cube.rotation.x = 0.15 * elapsedTime;
 
-  sphere.position.x = Math.cos(elapsedTime);
-  sphere.position.y = Math.sin(elapsedTime);
+    sphere.position.x = Math.cos(elapsedTime);
+    sphere.position.y = Math.sin(elapsedTime);
 
-  // Update controls
-  controls.update();
+    // Update controls
+    controls.update();
 
-  // Render
-  renderer.render(scene, camera);
+    // Render
+    renderer.render(scene, camera);
 
-  // Call tick again on the next frame
-  window.requestAnimationFrame(tick);
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick);
 };
 
 tick();
